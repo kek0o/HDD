8456f857c9d283700b3e3d1db15e163b129ad68d
Sergi Hernandez Gonzalez <sergi@Air-de-Sergi-4.home>
2024-09-23 19:59:05 +0200

    37 registers radix-4 booth algorithm implementation

--- lab4/booth.v
+++ lab4/booth.v
@@ -14,75 +14,64 @@ input irq_enable // enable for output irq
 );
 
 reg signed [31:0] partial_res;
-
+reg[3:0] window_count;
+reg state;
 reg [2:0] window;
-reg[2:0] window_count;
-reg [1:0] state;
-reg prev;
 
+assign window = partial_res[2:0];
+assign busy = state;
 always @(posedge clk) begin
   if (!resetn) begin
-    busy <= 1'b0;
     irq <= 1'b0;
-    result <= 32'b0;
     partial_res <= 32'b0;
-    prev <= 1'b0;
-    window_count <= 0;
-    window <= 3'b0;
-    state  <= 2'b00;
+    window_count <= 4'b0;
+    state <= 1'b0;
   end else begin
     case (state)
-      2'b00: begin // idle
+      1'b0: begin //idle
         if (start) begin
-          busy <= 1'b1;
-          window <= {data_b[1:0], prev};
-          state <= 2'b01;
+          partial_res <= {15'b0, data_b, 1'b0};
+          state <= 1'b1;
         end
       end
-      2'b01: begin // mult
-        case (window) // actions for the radix-4 booth multiplier
-          3'b000: partial_res <= partial_res;
-          3'b001: partial_res <= partial_res + data_a;
-          3'b010: partial_res <= partial_res + data_a;
-          3'b011: partial_res <= partial_res + (data_a << 1);
-          3'b100: partial_res <= partial_res - (data_a << 1);
-          3'b101: partial_res <= partial_res - data_a;
-          3'b110: partial_res <= partial_res - data_a;
-          3'b111: partial_res <= partial_res;
-        endcase
-
-        if (window_count == 7) begin // 
-          result <= partial_res;
-          window_count <= 2'b0;
-          prev <= 1'b0;
-          irq <= irq_enable ? 1'b1 : 1'b0;
-          state <= 2'b10;
-        end else begin
-          data_b <= data_b >>> 2;
-          prev <= data_b[1];
+      1'b1: begin // busy
+        if (window_count < 8) begin
+          case (partial_res[2:0]) // radix-4 booth multiplier decoder
+            3'b001: partial_res = (partial_res + {data_a,16'b0}); // + data_a
+            3'b010: partial_res = (partial_res + {data_a,16'b0}); // +data_a
+            3'b011: partial_res = (partial_res + ({data_a <<< 1,16'b0})); // +2*data_a
+            3'b100: partial_res = (partial_res - ({data_a <<< 1,16'b0})); // -2*data_a
+            3'b101: partial_res = (partial_res - {data_a, 16'b0}); // -data_a
+            3'b110: partial_res = (partial_res - {data_a,16'b0}); // -data_a
+            default: partial_res = partial_res;
+          endcase
+          partial_res <= partial_res >>> 2; // 2-bit shift right
           window_count <= window_count + 1'b1;
-          state <= 2'b11; // new window + shift
-        end
-      end
-      2'b10: begin // done
-        if (irq_enable) begin
-          if (ack) begin
-            irq <= 1'b0;
-            busy <= 1'b0;
-            state <= 2'b00;
-          end else state <= 2'b10;
         end else begin
-          busy <= 1'b0; 
-          state <= 2'b00;
+          if (irq_enable) begin
+            if (!ack) irq <= 1'b1;
+            else begin
+              irq <= 1'b0;
+              window_count <= 4'b0;
+              state <= 1'b0;
+            end
+          end else begin
+            window_count <= 4'b0;
+            state <= 1'b0;
+          end
         end
-        partial_res <= 32'b0;
-      end
-      2'b11: begin // new window + shift
-        window <= data_b{window_count*2+1-:3}
-        data_a <= data_a <<< 2*window_count;
-        state <= 2'b01;
       end
     endcase
+  end
+end
+
+always @(negedge clk) begin
+  if (!resetn) result = partial_res;
+  else begin
+    if (window_count == 8) begin
+      result = partial_res;
+      irq <= irq_enable;
+    end
   end
 end
 
--- lab4/booth_tb.v
+++ lab4/booth_tb.v
@@ -29,47 +29,60 @@ begin
   data_b = multiplier;
   @(posedge clk);
   #1;
-  start = 1'b0;
 end
 endtask
 
 task wait_ack();
 begin
-  wait(!busy);
-  @(posedge clk);
+  if (irq_enable) begin
+    wait (irq) begin
+      @(posedge clk);
+      #1;
+      ack = 1'b1;
+      @(posedge clk);
+      #1;
+      ack = 1'b0;
+    end
+  end else wait (!busy);
   #1;
-  ack = 1'b1;
+  start = 1'b0;
   @(posedge clk);
-  #1;
-  ack = 1'b0;
+  @(posedge clk);
 end
 endtask
 
+reg [1:0] count;
 //stimuli generation
 initial begin
   resetn = 1'b1;
   start = 1'b0;
   ack = 1'b0;
-  irq_enable = 1'b0;
+  irq_enable = 1'b1;
+
+  count = 0;
 
   #5 resetn = 1'b0;
   @(posedge clk);
   #1 resetn = 1'b1;
 
-  set_operands(2,16);
-  wait_ack();
-  set_operands(-7,-124);
-  wait_ack();
-  set_operands(150,-37);
-  wait_ack();
-  set_operands(-670,2);
-  wait_ack();
-  set_operands({16{1'b1}},{16{1'b1}});
-  wait_ack();
-  set_operands({1'b0,{15{1'b1}}},{1'b0,{15{1'b1}}});
-  wait_ack();
-  set_operands({1'b1,{15'b0}},{1'b1,15'b0});
-  wait_ack();
+  while (count <= 1) begin
+    set_operands(2,16);
+    wait_ack();
+    set_operands(-8,-124);
+    wait_ack();
+    set_operands(150,-38);
+    wait_ack();
+    set_operands(-670,2);
+    wait_ack();
+    set_operands({16{1'b1}},{16{1'b1}});
+    wait_ack();
+    set_operands({1'b0,{15{1'b1}}},{1'b0,{15{1'b1}}});
+    wait_ack();
+    set_operands({1'b1,{15'b0}},{1'b1,15'b0});
+    wait_ack();
+    count = count + 1;
+    irq_enable = 1'b0;
+  end
   #500 $finish;
 end
 
